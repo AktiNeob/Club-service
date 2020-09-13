0250ff60239870a38a1454e369aee3431c0f2df2
DarkNukename <kardinallektor@gmail.com>
2020-09-13 13:00:29 +0300

    fix

--- service/settings.py
+++ service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j5e++182xivs@vxkpv@*7hitn3eu=al=b!bnq-=51*@ghd0k_='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['secure-brook-79251.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
